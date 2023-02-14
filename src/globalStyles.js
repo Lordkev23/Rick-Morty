3ec92c96ffb0d918375d6164d718ce78152ad4be
Kevin <kevdaviman.23@gmail.com>
2023-02-14 01:29:10 -0500

    up2

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -3,8 +3,11 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
 body{
-    background: url('https://cdn.dribbble.com/users/1818304/screenshots/6601989/rick__dribbble.gif');
-    background-size: 64.5%;
+    background: url('https://static1.srcdn.com/wordpress/wp-content/uploads/2019/07/Rick-and-Morty-Squanchy-Gifts.jpg');
+    background-size: 100%;
     background-attachment: fixed;
 }
 `
+
+// https://cdn.dribbble.com/users/1818304/screenshots/6601989/rick__dribbble.gif
+// https://static1.srcdn.com/wordpress/wp-content/uploads/2019/07/Rick-and-Morty-Squanchy-Gifts.jpg
