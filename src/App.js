baa19f0a266ec55900237af64c5cd93c5cc3899e
Kevin <kevdaviman.23@gmail.com>
2023-02-24 18:31:57 -0500

    folder Cards created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import Cards from './components/Cards.jsx'
+import Cards from './components/Cards/Cards.jsx'
 import styled from 'styled-components'
 import GlobalStyles from './globalStyles'
 import { useState, useEffect } from 'react'
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './Card/Card';
+import Card from '../Card/Card';
 import styled from 'styled-components';
 
 const DivCards = styled.div`
